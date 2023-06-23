f5dcaacc9668bb6f6907d623b666a598945d7478
farci5443 <137557513+farci5443@users.noreply.github.com>
2023-06-24 01:16:36 +0600

    Add files via upload

---
+++ part1.py
@@ -0,0 +1,65 @@
+#-----------Main-----------#
+import os,sys,time,random,string
+import json.platform
+#-----------import-----------#
+try:
+	import requests
+except importError:
+	os.system('pip install requests')
+	import mechanize
+except importError:
+	os.system('pip install mechanize')
+	import bs4
+except importError:
+	os.system('pip install bs4')
+#-----------xnx-----------#
+from concurrent.futures import ThreadPoolExecutor as xnx
+from bs4 import beautifulsoup as sop
+
+#-----------colour-----------#
+green='\033[1:32m'
+red='\003[1:31m'
+yellow='\033[1:33m'
+blue='\033[1:34m'
+white='\033[1:37m'
+
+#-----------user----------#
+user[ ]
+
+
+#-----------logo-----------#
+def logo( ):
+	os.system('clear')
+	print(f"""{white}
+     _____  _  __      _   
+    |  __ \(_)/ _|    | |  
+    | |__) |_| |_ __ _| |_ 
+    |  _  /| |  _/ _` | __|
+    | | \ \| | || (_| | |_ 
+    |_|  \_\_|_| \__,_|\__|
+    
+    --------------------------------------------------------------------------------
+    {white}[{green}+{white}]  FACEBOOK  :Rifat Hasan
+   {white}[{green}+{white}]  GITHUB        :legend544
+   {white}[{green}+{white}]  QUALITY      :TESTING
+   ---------------------------------------------------------------------------------""")
+   
+#-----------Rifat_main-----------#
+def main_Rifat( ) :
+       logo( ) 
+       code=input(f'{white}[{green}+{white}] BD CODE : {green}017,018,019\n{white}[{green}+{white}]    SIM CODE :  ')
+       limit=int(input(f'{white}[{green}+{white}] CRACK LIMIT  : {green}10000,20000,30000\n{white}[{green}+{white}]  CRACK ID : '))
+       for x in range(limit):
+       	sex=' '.join(random.choice(strings.digits) for _ in range(8)
+           user. append(sex)
+       with xnx(max_workers=30) as tanisha:
+       	all=str(len(user))
+           print(f'{white}[{green}+{white}] YOUR CHOICE SIM CODE  : {green}{code}_')
+           print(f'{white}[{green}+{white}] TOTAL ID CRACKING          : {blue} {all} ') 
+           print(f'{white}[{green}+{white}] {yellow}USE MOBILE DATA FOR MORE OK IDZ ')
+           print(f'{white}--------------------------------------------------------------------------------- :  ')
+try:
+      main_Rifat( ) 
+except:
+    main_Rifat( ) 
+   
